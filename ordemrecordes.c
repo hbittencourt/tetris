5f99fe4417740f411ae35b78f81eb310bfb9492c
VICTOR VARALLO FERREIRA <16104092@l1847151.portoalegre.pucrsnet.br>
2016-06-09 11:29:10 -0300

    09/06/16

--- ordemrecordes.c
+++ ordemrecordes.c
@@ -2,54 +2,89 @@
 //opção 2-recordes
 #include <stdio.h>
 #include <string.h>
+#define SIZE 11
+	
+/**************************************************************************************************************/
+//
+//		concertar leitura da string do arquivo nomes.txt e armazena-la na struct junto com o score para
+//      posterior ordenacao
+//
+//
+//
+//
+/***************************************************************************************************************/
 
-/*
-struct stscore{			//struct deve ler recordes armazenados no arquivo recordes.txt
-	char nome[50];
-	int score;
-}scores[10];
+struct stscore{
+//char anome[50];
+int ascore;
+}score[SIZE];
 
-struct snome{
-	char vnome[50];
-}nomes[10];
-*/
+struct auxscore{
+//char anome[50];
+int ascore;
+}aux;
+
 int main(void)
 {
 	FILE *arquivo;
-	int temp,pts,i,points[11]={0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	char name[50];
-
-	
+	int j,k;
+//	char nome[50];
+//	char *nome;	
 
 	arquivo = fopen("recordes.txt","r");
 	
-	while(fscanf(arquivo,"%d",&pts)!= 1)				//  while (!feof(arq))
+	k=0;
+	//le o arquivo de recordes e preenche os ascore na struct
+	while (!feof(arquivo))
 	{
-	for(i=0;i<10;i++)
-		{
-		if(pts>points[i])	
-			{
-			temp=points[i];
-			points[i]=pts;
-			points[i+1]=temp;
-			break;
-			}
-		}
+		fscanf(arquivo,"%d",&score[k].ascore);
+		k++;
+	}
 	
-	}
-
 	fclose(arquivo);
-/*
-	for(i=0;i<10;i++)
-	{
-		scores[i].nome=nomes[i].vnome;
-		scores[i].score=points[i];
-	}
-*/
-	printf("pontuacao\n");
 
-	for(i=1;1<10;i++)
-		printf("%d\n",points[i]);
+
+/*	//arrumar leitura da string
+	arquivo = fopen("nomes.txt","r");
+	
+	k=0;
+	//le o arquivo com os nomes e preenche os anome na struct
+	while (!feof(arquivo))
+	{
+		score[k].anome = fgets(nome, 50, arquivo);
+	//	score[k].anome=nome;
+		k++;
+	}
+	
+	fclose(arquivo);
+*/
+/**********************************
+
+//ordena struct de acordo com a pontuacao
+	for(k=0;k<SIZE-1;k++)
+	{
+		aux.anome=score[k].anome;
+		aux.ascore=score[k].ascore;
+		for(j=k+1;j<SIZE;j++)
+		{
+			if(score[j].ascore>score[k].ascore)
+			{
+				aux.anome=score[k].anome;
+				aux.ascore=score[k].ascore;
+				score[k].anome=score[j].anome;
+				score[k].ascore=score[j].ascore;
+				score[j].anome=aux.anome;
+				score[j].ascore=aux.ascore;
+			}	
+		}
+	}
+***********************************/
+
+
+	printf("pontuacao\n");		//printa recordes
+	for(k=1;1<10;k++)
+		//printf("%s\n",score[k].anome);
+		printf("%d\n",score[k].ascore);
 	
 	return 0;
 }	
