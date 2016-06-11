bd91aece403e0e78165f75c44cc4fa952b6e90ee
victorvarallo <victor.varallo@acad.pucrs.br>
2016-06-11 11:11:04 -0300

    inserida no menu a opcao 2-recordes

--- tetris_provisorio.c
+++ tetris_provisorio.c
@@ -33,6 +33,18 @@
 #include "conio.h"
 #include "tetris.h"
 
+#define SIZE 9999999
+
+struct stscore{
+char anome[50];
+int ascore;
+}score[SIZE];
+
+struct auxscore{
+char anome[50];
+int ascore;
+}aux;
+
 static struct termios savemodes;
 static int havemodes = 0;
 
@@ -89,6 +101,65 @@ int shapes[] = {
     6,  TC,  BC,  2 * B_COLS,   /* sticks out */
 };
 
+void recordes(void)
+{
+	FILE *arquivo;
+	int j,k,count=0;
+	char nome[50];
+
+	arquivo = fopen("recordes.txt","r");
+	
+	k=0;
+	//le o arquivo de recordes e preenche os ascore na struct
+	while (!feof(arquivo))
+	{
+		fscanf(arquivo,"%d",&score[k].ascore);
+		k++;
+		count++;
+	}
+	
+	fclose(arquivo);
+
+	arquivo = fopen("nomes.txt","r");  
+	
+	k=0;
+	//le o arquivo com os nomes e preenche os anome na struct
+	while (!feof(arquivo))
+	{
+        fscanf(arquivo, "%s",nome);
+        strcpy(score[k].anome, nome);
+		k++;
+	}
+	
+	fclose(arquivo);
+
+
+
+//ordena struct de acordo com a pontuacao
+	for(k=0;k<count-1;k++) //for executa tantas vezes qunto o numero de linhas(count) -1
+	{
+	//copia a struct para uma auxiliar
+	strcpy(aux.anome, score[k].anome);//aux.anome=score[k].anome;
+	aux.ascore=score[k].ascore;
+		for(j=k+1;j<count;j++)//compara uma struct com a seguinte no vetor
+		{
+			if(score[j].ascore>score[k].ascore)
+			{
+				strcpy(aux.anome, score[k].anome);//aux.anome=score[k].anome;
+				aux.ascore=score[k].ascore;
+				strcpy(score[k].anome, score[j].anome);//score[k].anome=score[j].anome;
+				score[k].ascore=score[j].ascore;
+				strcpy(score[j].anome, aux.anome);//score[j].anome=aux.anome;
+				score[j].ascore=aux.ascore;
+			}	
+		}
+	}
+
+
+	printf("pontuacao:\n");		//printa top 10
+	for(k=0;k<10;k++)
+		printf("%do : \t%d\t-%s\n",k+1,score[k].ascore,score[k].anome);
+}	
 
 void alarm_handler (int signal __attribute__ ((unused)))
 {
@@ -235,7 +306,7 @@ void game_over()
 //Arquivo de Recordes
 
 
-void arq(char jogador[])
+void arq(char jogador[], int points, int level)
 
 {
     FILE *arq;
@@ -494,7 +565,7 @@ jogo:
 		game_over();
 
                 printf ("Your score: %d points x level %d = %d\n\n", points, level, points * level);
-                arq(player);
+                arq(player,points,level);
                 break;
             }
 
